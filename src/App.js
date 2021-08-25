0e609afbc0caca8cfe9e2ea1faaa06cf96fc50eb
EUNHEE JANG <6810779s@naver.com>
2021-08-26 08:45:25 +0900

    add

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className={`container1 container2`}>
+      <h1>I have two Classes</h1>
+    </div>
+  );
+};
+
+export default App;
